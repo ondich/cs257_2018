4f63690421e1a7cdb0aa205f270ea0d33f3c7afd
Jeff Ondich <ondich@ultralingua.com>
2018-04-01 14:55:16 -0500

    Adjusted the constructor for Autocompleter.

--- Autocomplete/src/edu/carleton/jondich/Autocompleter.java
+++ Autocomplete/src/edu/carleton/jondich/Autocompleter.java
@@ -12,7 +12,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Autocompleter {
-    public Autocompleter() {
+
+    /**
+     * @param dataFilePath the path to the data file containing the set of items to
+     * from which auto-completed results will be drawn. (In the context of this assignment,
+     * this will be the path to the actors.txt file I provided you. But we'll also talk
+     * later about how you might use inheritance to create subclasses of Autocompleter
+     * to use different datasets and different approaches to doing the autocompletion.)
+     */
+    public Autocompleter(String dataFilePath) {
         // Initialization goes here, as needed. For example, you might load
         // from a file into a list (or a hashmap or something like that)
         // the list of strings that are going to form the dataset of potential
