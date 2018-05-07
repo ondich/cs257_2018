6e931150007ea726ae12904c94a925e94b154f05
Jeff Ondich <ondich@ultralingua.com>
2018-05-07 07:53:51 -0500

    Cleaned up the books/authors web application example. Less clutter, better API.

--- books/books_website.py
+++ books/books_website.py
@@ -20,14 +20,16 @@ app = flask.Flask(__name__, static_folder='static', template_folder='templates')
 @app.route('/') 
 def get_main_page():
     ''' This is the only route intended for human users '''
-    return flask.render_template('index.html')
+    global api_port
+    return flask.render_template('index.html', api_port=api_port)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
+    if len(sys.argv) != 4:
+        print('Usage: {0} host port api-port'.format(sys.argv[0]), file=sys.stderr)
         exit()
 
     host = sys.argv[1]
     port = sys.argv[2]
+    api_port = sys.argv[3]
     app.run(host=host, port=port)
 
